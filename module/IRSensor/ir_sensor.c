2c9bc40468463fce88580dfe45b608f557f08c04
nicholas.ykhuang@gmail.com <nicholas.ykhuang@gmail.com@d72f4762-8c98-daf9-36e7-2fa9b6f8901a>
2014-04-17 04:59:23 +0000

    Saved

--- module/IRSensor/ir_sensor.c
+++ module/IRSensor/ir_sensor.c
@@ -16,12 +16,16 @@
 #define SAMPLING_RATE 2000
 #define TIMESLICE 2*TIME_1MS  // thread switch time in system time units
 
+Sema4Type	Sema4IRDataAvailable;
+
 /********************** Data Structure *******************/
 #define FIFOSIZE   64         // size of the FIFOs (must be power of 2)
 #define FIFOSUCCESS 1         // return value on success
 #define FIFOFAIL    0         // return value on failure
                               // create index implementation FIFO (see FIFO.h)
-
+	long StartCritical(void);    // previous I bit, disable interrupts
+	void EndCritical(long sr);    // restore I bit to previous value
+	
 AddIndexSema4Fifo(IR1, FIFOSIZE, unsigned short, FIFOSUCCESS, FIFOFAIL)
 AddCallBackFunction(IR1)
 
@@ -42,50 +46,51 @@ const long H[51]={-11,10,9,-5,1,0,-19,6,48,-12,-92,
      8578,4932,24,-1584,-24,881,24,-559,-24,370,22,-243,-20,155,
      17,-92,-12,48,6,-19,0,1,-5,9,10,-11};
 
-typedef {
+typedef struct {
 	// this MACQ needs twice the size of FILTER_LENGTH
 	long x[2*FILTER_LENGTH];
 	unsigned char index;
 } FilterType;
 
-static FilterType filter1;
-filter1.index = FILTER_LENGTH-1; // to let pre-increment start at x[FILTER_LENGTH]
+static unsigned short IRsensor1;
+static unsigned short IRsensor2;
+static unsigned short IRsensor3;
+static unsigned short IRsensor4;
+
+static FilterType filter1 = {{0},FILTER_LENGTH-1};
 
 #if LAB_DEMO == 7
 
-static FilterType filter2;
-filter2.index = FILTER_LENGTH-1; // to let pre-increment start at x[FILTER_LENGTH]
-static FilterType filter3;
-filter3.index = FILTER_LENGTH-1; // to let pre-increment start at x[FILTER_LENGTH]
-static FilterType filter4;
-filter4.index = FILTER_LENGTH-1; // to let pre-increment start at x[FILTER_LENGTH]
+static FilterType filter2 = {{0},FILTER_LENGTH-1};
+static FilterType filter3 = {{0},FILTER_LENGTH-1};
+static FilterType filter4 = {{0},FILTER_LENGTH-1};
 
 #endif
 
 // Filter_ClearMemo
 // this is called when one stops using the filter
 // such that when filter is being used again the filter memory will be fresh
-static void Filter_ClearMemo(FilterType *f) {
-  unsigned char i;
-  for(i = 0; i < 2*FILTER_LENGTH; ++i) f->x[i] = 0;
-  f.index = FILTER_LENGTH-1; // to let pre-increment start at x[FILTER_LENGTH]
-}
+//static void Filter_ClearMemo(FilterType *f) {
+//  unsigned char i;
+//  for(i = 0; i < 2*FILTER_LENGTH; ++i) f->x[i] = 0;
+//  f->index = FILTER_LENGTH-1; // to let pre-increment start at x[FILTER_LENGTH]
+//}
 
 // Filter
 // Digital FIR filter, assuming fs=1 Hz
 // Coefficients generated with FIRdesign64.xls
 // 
 // y[i]= (h[0]*x[i]+h[1]*x[i-1]+…+h[63]*x[i-63])/256;
-static long Filter(FilterType *f, long data) {
+static unsigned short Filter(FilterType *f, unsigned short data) {
   long y = 0;
   unsigned char i;
   
   if(++f->index == 2*FILTER_LENGTH) f->index = FILTER_LENGTH;
-  f->x[n] = f->x[n-FILTER_LENGTH] = data;
+  f->x[f->index] = f->x[f->index-FILTER_LENGTH] = data;
   
   // Assuming there is no overflow
   for(i = 0; i < FILTER_LENGTH; ++i){
-		y += H[i]*f->x[n-i];
+		y += H[i]*f->x[f->index-i];
 	}
   y /= ScaleFactor;
   
@@ -115,37 +120,17 @@ static void Consumer(void) {
 
   while (1) {
 		// Get data, will block if FIFO is empty
-		long data = IR1Fifo_Get();
-			
-		// Choosing whether to apply the filter
-		if(Filter_Use == NOW_USING) data = Filter(&filter1, data);
-		else if(Filter_Use == STOP_USING) {
-			Filter_Use = NOT_USING;
-			Filter_ClearMemo(&filter1);
-		}
+		unsigned short data;
+		IR1Fifo_Get(&data);
 		
-		// record data
-		recording[puti] = recording[puti+FFT_LENGTH] = data;
-		puti = (puti + 1) % FFT_LENGTH;
-		
-		if(puti == geti) { // a data point is just put in, so in this case it is overrun
-		  DataLost += 1;
-		  geti = (geti + 1) % FFT_LENGTH; // discard the oldest value
-		} else {
-			if (DisplayMode == TIME ) {			
-				OS_Signal(&Sema4DataAvailable);
-			}
-		}
+		// Apply filter
+		IRsensor1 = Filter(&filter1, data);
 	}
 }
 
-#undef NOW_USING
-#undef STOP_USING
-#undef NOT_USING
 
-
-int IR_Init(void) {
-  OS_InitSemaphore(&Sema4DataAvailable, 0);
+void IR_Init(void) {
+  OS_InitSemaphore(&Sema4IRDataAvailable, 0);
   
   IR1Fifo_Init();
   
@@ -156,4 +141,13 @@ int IR_Init(void) {
 #endif
 
   OS_AddThread(&Consumer, 256, 3);
+}
+
+void IR_getValues (unsigned short *buffer) {
+	buffer[0] = IRsensor1;
+	#if LAB_DEMO == 7
+	buffer[1] = IRsensor2;
+	buffer[2] = IRsensor3;
+	buffer[3] = IRsensor4;
+	#endif
 }
